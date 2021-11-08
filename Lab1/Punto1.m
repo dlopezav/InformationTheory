00ec86a37e1dba97349f4cff2864edbfbf4af3ea
Diego Felipe <dlopezav@unal.edu.co>
2021-11-08 16:00:27 -0500

    arreglos2

--- Lab1/Punto1.m
+++ Lab1/Punto1.m
@@ -1,23 +1,23 @@
-xt = input('Ingrese la función x(t) = ', 's');
-T = input('Ingrese el periodo = ');
-k = input('Ingrese la cota N = ');
+syms x
 a = input('Ingrese el punto de inicio = ');
-f = inline(xt, 't');
+k = input('Ingrese la cota N = ');
 
 t = [a:0.001:T+a];
-
-a_0=2/T*quad(f,a, a+T);
+f = @(t) double(t>=pi);
+ 
+a_0=2/T*integral(f,a, a+T);
 
 w=2*pi/T;
 
 xt1 = a_0/2;
-
-a_n = @(x) (2/T).*integral(@(ti) xt.*cos(x*w*ti),a,T+a);
-b_n = @(x) (2/T).*integral(@(ti) xt.*sin(x*w*ti),a,T+a);
+a_n = @(x) (2/T).*integral(@(ti) f(ti).*cos(x*w*ti),a,T+a);
+b_n = @(x) (2/T).*integral(@(ti) f(ti).*sin(x*w*ti),a,T+a);
 
 for i = 1 : k
     xt1= xt1 + a_n(i)*cos(w*i*t)+b_n(i)*sin(w*i*t);
 end
+
+
 p = plot(t,f(t),'-', t,xt1,'-r');
 p(1).LineWidth = 2;
 p(2).LineWidth = 2;
