1cb827c4ac8fec797a9515a5f58fa0a07e4a7e89
ogomezs <ogomezs@unal.edu.co>
2021-11-07 19:59:11 -0500

    Puntos 2 completos
    Esto es una descripción

---
+++ Lab1/Filters.m
@@ -0,0 +1,82 @@
+[X, t,f0,fs,Ts, f] = Punto2;
+wc = input('Ingrese el valor de wc = ');
+wm = input('Ingrese el valor de wm = ');
+
+
+H_LPF = @(t) heaviside(t+wc) - heaviside(t-wc);
+H_LPF_2 = @(t) (abs(t) <= wc);
+H_HPF = @(t) (abs(t) >= wc);
+H_BPF = @(t) (wc <= abs(t) & abs(t) <= wm);
+H_BSF = @(t) ~(wc <= abs(t) & abs(t) < wm);
+
+%*******************************
+
+
+%Subplot Frecuencia total
+subplot(6,2,[1 , 2]);
+stem(f-(0.5),fftshift(abs(X)),'-b')
+
+subplot(6,2,[3, 4]);
+stem(t,abs(ifft(ifftshift(X./Ts))))
+
+%*******************************
+%Subplot LPF 
+lowFilter = fftshift(abs(X)).*H_LPF_2(f-(0.5));
+subplot(6,2,5);
+stem(f-(0.5),lowFilter)
+
+%Subplot LPF Pulse
+subplot(6,2,6);
+stem(t,abs(ifft(ifftshift(lowFilter./Ts))))
+
+%******************************
+%Subplot HPF 
+highFilter = fftshift(abs(X)).*H_HPF(f-(0.5));
+subplot(6,2,7);
+stem(f-(0.5),highFilter)
+
+%Subplot HPF Pulse
+subplot(6,2,8);
+stem(t,abs(ifft(ifftshift(highFilter./Ts))))
+
+
+%******************************
+%Subplot BPF 
+PassBandFilter = fftshift(abs(X)).*H_BPF(f-(0.5));
+subplot(6,2,9);
+stem(f-(0.5),PassBandFilter)
+
+%Subplot BPF Pulse
+subplot(6,2,10);
+stem(t,abs(ifft(ifftshift(PassBandFilter./Ts))))
+
+%******************************
+%Subplot BSF 
+bandStopFilter = fftshift(abs(X)).*H_BSF(f-(0.5));
+subplot(6,2,11);
+stem(f-(0.5),bandStopFilter)
+
+%Subplot BSF Pulse
+subplot(6,2,12);
+stem(t,abs(ifft(ifftshift(bandStopFilter./Ts))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lab1/Punto2.m
+++ Lab1/Punto2.m
@@ -1,9 +1,14 @@
+function [X, t, f0, fs, Ts, f] = Punto2()
 a = input('Ingrese el valor de a = ');
 T=2*a;                            % duración de la señal
 f0=1;                             % valor de la frecuencia lineal
 fs=50*f0; Ts=2*a/fs;                % intervalo de muestreo y frecuencia de muestreo
 t= [-2*a:Ts:2*a];                 % vector de tiempo
-x= (abs(t)< a); X=fft(x)*Ts;      % pulso rectangular
+x= (abs(t)<= a); X=fft(x)*Ts;
+Xr = X./Ts;
+length(X)
+length(Xr)
+% pulso rectangular
 f= [-length(t)/2+1:length(t)/2];  % vector de frecuencias
 subplot(2,1,1); stem(t,x,'-r'); axis([-3*a 3*a -1 1.5]); xlabel('nT_s'); ylabel('x(nT_s)');
 title(strcat('Onda rectangular con a = ', num2str(a)));
@@ -19,3 +24,6 @@ grid;
 ax = gca;
 ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
+
+end
+
