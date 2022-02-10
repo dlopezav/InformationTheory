b33d359649230ca2be465557e14e8b49f6231c59
Diego Felipe <dlopezav@unal.edu.co>
2022-02-10 00:33:03 -0500

    1st part

---
+++ Project/energy.m
@@ -0,0 +1,4 @@
+function f = energy(T)
+    k = 1.38064852*10^-23;
+    f = k * log(2) * T;
+end
---
+++ Project/variation.m
@@ -0,0 +1,8 @@
+function res = EntropyTermo(tiempo, varHeat, iniHeat)
+    for i = 1:tiempo
+        ener = energy(iniHeat);
+        
+        grid on
+        plot(t,x);
+    end
+end
